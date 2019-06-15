bf85ca3c4f282e7f0e19f36883e667dc30759877
jtuttas <jtuttas@gmx.net>
2019-06-15 11:54:54 +0200

    Findet Automatisch den Server auf dem die App läuft

--- src/app/data/Config.ts
+++ src/app/data/Config.ts
@@ -1,7 +1,7 @@
 export class Config {
   static debug: boolean = false;
-  static version:string="1.17";
-  public static SERVER:string = "http://localhost:8080/";
+  static version:string="2.0";
+  public static SERVER:string = "http://"+window.location.hostname+":8080/";
 }
 
 
